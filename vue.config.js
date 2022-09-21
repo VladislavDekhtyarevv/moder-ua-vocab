9a33f855836f054dde72f9ca0857ea6b6c3fbfd6
Vladislav Dehtarev <dehtarevvladislav@gmail.com>
2022-09-21 05:28:23 +0300

    fix deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    lintOnSave: false
+    lintOnSave: false,
+    devServer: {
+        disableHostCheck: true
+    }
 })
